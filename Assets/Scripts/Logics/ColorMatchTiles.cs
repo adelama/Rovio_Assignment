294b877fb42a14be5b6dd87f8ca55b27342da122
adel <adel.darvishi@gmail.com>
2022-09-11 00:04:45 +0430

    implementing ColorMatchTiles model

--- Assets/Scripts/Logics/ColorMatchTiles.cs
+++ Assets/Scripts/Logics/ColorMatchTiles.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Rovio.TapMatch.Logic
 {
     public class ColorMatchTiles
-    { 
+    {
+        private HashSet<int> tileIndices;
 
+        public int[] TileIndices => tileIndices.ToArray();
+
+        public ColorMatchTiles()
+        {
+            tileIndices = new HashSet<int>();
+        }
+
+        public void Clear()
+        {
+            tileIndices.Clear();
+        }
+
+        public bool HasTile(int tileIndex)
+        {
+            return tileIndices.Contains(tileIndex);
+        }
+
+        public void AddTile(int tileIndex)
+        {
+            tileIndices.Add(tileIndex);
+        }
     }
 }
