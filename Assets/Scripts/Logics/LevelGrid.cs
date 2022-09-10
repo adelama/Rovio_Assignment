b5405f2f6fa2a37627be593cd17e169e561cec08
adel <adel.darvishi@gmail.com>
2022-09-10 16:48:45 +0430

    adding logic required model classes

---
+++ Assets/Scripts/Logics/LevelGrid.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Rovio.Logic
+{
+    public class LevelGrid
+    { 
+
+
+    }
+}
---
+++ Assets/Scripts/Logics/LogicController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Rovio.Logic
+{
+    public class LogicController
+    { 
+
+    }
+}
