b2cd68be64b718616e1a67a905876176e4f950bb
adel <adel.darvishi@gmail.com>
2022-09-10 17:00:09 +0430

    adding deterministic number random generator

---
+++ Assets/Scripts/Logics/RandomGenerator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rovio.Logic
+{
+    public class RandomGenerator
+    {
+        private const uint RangeMax = 0x7fffffffU;
+
+        private uint m_Seed;
+
+        public RandomGenerator(uint seed)
+        {
+            m_Seed = seed & RangeMax;
+        }
+
+        public RandomGenerator(int seed)
+        {
+            Seed = ((uint)seed);
+        }
+
+        public uint Seed
+        {
+            get { return m_Seed; }
+            set { m_Seed = value & RangeMax; }
+        }
+
+        public int Next()
+        {
+            m_Seed = (m_Seed * 1103515245U + 12345U) & RangeMax;
+            return (int)m_Seed;
+        }
+
+        /// <summary>
+        /// returns in [0, maxValue) range
+        /// </summary>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public int Next(int maxValue)
+        {
+            return (int)((long)Next() * maxValue / RangeMax);
+        }
+
+        /// <summary>
+        /// [minValue, maxValue)
+        /// </summary>
+        /// <param name="minValue"></param>
+        /// <param name="maxValue"></param>
+        /// <returns></returns>
+        public int Next(int minValue, int maxValue)
+        {
+            if (minValue == maxValue) return maxValue;
+            var diff = maxValue - minValue;
+            return Next(diff) + minValue;
+        }
+    }
+}
