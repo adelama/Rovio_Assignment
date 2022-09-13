d3b28fc32a7870490e77acfa3344ce4b6634e57a
adel <adel.darvishi@gmail.com>
2022-09-13 12:21:08 +0430

    adding widows form project to handle remote controller of game

---
+++ TapMatchWindowsAppProject/RemoteController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rovio.TapMatch.WindowsApp
+{
+    internal class RemoteController
+    {
+
+    }
+}
---
+++ TapMatchWindowsAppProject/RemoteForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rovio.TapMatch.WindowsApp
+{
+    public partial class RemoteForm : Form
+    {
+        public RemoteForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RemoteForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
